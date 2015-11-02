c476109115f342cd098644febdf4e68b9c2dbb35
Jason W. May <jmay@pobox.com>
2015-11-02 13:20:19 -0800

    I want customization settings persisted, just not secret private stuff

---
+++ personal/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js2-basic-offset 2)
+ '(js2-bounce-indent-p t)
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/Documents/Notes/veriphyr.org" "~/Dropbox/Documents/Notes/education.org" "~/Dropbox/Documents/Notes/career_personal_goals.org" "~/Dropbox/Documents/Notes/arbuelo_house_fixtures_appliances_utilities.org" "~/Dropbox/Documents/Notes/emacs-todo.org" "~/Dropbox/Documents/Notes/basis_school.org" "~/Dropbox/Documents/Notes/00 TODO & REMINDERS.org")))
+ '(org-startup-folded (quote showeverything))
+ '(org-startup-indented t)
+ '(paradox-github-token "c9076cfc5ad03fe402762c707549d0bf624e8a96"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil)))))
