b7d17319773fd2f94e241dcf06160a3c4a23c592
Jason W. May <jmay@pobox.com>
2015-12-05 16:29:55 -0800

    latest customization

--- personal/custom.el
+++ personal/custom.el
@@ -3,15 +3,87 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(fci-rule-color "#073642")
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#002b36" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+   (quote
+    (("#073642" . 0)
+     ("#546E00" . 20)
+     ("#00736F" . 30)
+     ("#00629D" . 50)
+     ("#7B6000" . 60)
+     ("#8B2C02" . 70)
+     ("#93115C" . 85)
+     ("#073642" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
+ '(hl-fg-colors
+   (quote
+    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
  '(js2-basic-offset 2)
  '(js2-bounce-indent-p t)
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   (quote
+    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(org-agenda-files
    (quote
-    ("~/.emacs.d/personal/50_myemacs.org" "~/Dropbox/Documents/Notes/veriphyr.org" "~/Dropbox/Documents/Notes/education.org" "~/Dropbox/Documents/Notes/career_personal_goals.org" "~/Dropbox/Documents/Notes/arbuelo_house_fixtures_appliances_utilities.org" "~/Dropbox/Documents/Notes/emacs-todo.org" "~/Dropbox/Documents/Notes/basis_school.org" "~/Dropbox/Documents/Notes/00 TODO & REMINDERS.org")))
+    ("~/Dropbox/Documents/Notes/May Family Foundation MFF.org" "~/.emacs.d/personal/50_myemacs.org" "~/Dropbox/Documents/Notes/veriphyr.org" "~/Dropbox/Documents/Notes/education.org" "~/Dropbox/Documents/Notes/career_personal_goals.org" "~/Dropbox/Documents/Notes/arbuelo_house_fixtures_appliances_utilities.org" "~/Dropbox/Documents/Notes/emacs-todo.org" "~/Dropbox/Documents/Notes/basis_school.org" "~/Dropbox/Documents/Notes/00 TODO & REMINDERS.org")))
  '(org-confirm-babel-evaluate nil)
  '(org-startup-folded (quote showeverything))
  '(org-startup-indented t)
- '(paradox-github-token "c9076cfc5ad03fe402762c707549d0bf624e8a96"))
+ '(paradox-github-token "c9076cfc5ad03fe402762c707549d0bf624e8a96")
+ '(pos-tip-background-color "#073642")
+ '(pos-tip-foreground-color "#93a1a1")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#dc322f")
+     (40 . "#ff7f00")
+     (60 . "#ffbf00")
+     (80 . "#b58900")
+     (100 . "#ffff00")
+     (120 . "#ffff00")
+     (140 . "#ffff00")
+     (160 . "#ffff00")
+     (180 . "#859900")
+     (200 . "#aaff55")
+     (220 . "#7fff7f")
+     (240 . "#55ffaa")
+     (260 . "#2affd4")
+     (280 . "#2aa198")
+     (300 . "#00ffff")
+     (320 . "#00ffff")
+     (340 . "#00ffff")
+     (360 . "#268bd2"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
+ '(xterm-color-names
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
+ '(xterm-color-names-bright
+   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
